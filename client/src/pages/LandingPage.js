f547ccea7d7e0b8532703e831c899a8f9eb6f40e
FireAstrox <rtclavell2@alaska.edu>
2024-10-11 14:26:36 -0800

    Cleaned up the Landing page a bit

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -5,20 +5,65 @@ import { Container, Typography, Button, Box } from '@mui/material';
 
 function LandingPage() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ mt: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography variant="h2" component="h1" gutterBottom>
-          Welcome to CaneBane
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          Your personal Kanban board manager
-        </Typography>
-        <Button variant="contained" color="primary" component={RouterLink} to="/signup" sx={{ mt: 4 }}>
-          Get Started
-        </Button>
-      </Box>
-    </Container>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        justifyContent: 'center',
+        backgroundColor: '#f5f5f5', // Light grey background
+      }}
+    >
+      <Container maxWidth="md">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            textAlign: 'center',
+            marginTop: '-20vh',
+          }}
+        >
+          <Typography
+            variant="h2"
+            component="h1"
+            gutterBottom
+            sx={{
+              fontWeight: 'bold',
+              color: 'primary.main',
+            }}
+          >
+            Welcome to CaneBane
+          </Typography>
+          <Typography
+            variant="h5"
+            component="h2"
+            gutterBottom
+            sx={{
+              mb: 4,
+              color: 'text.secondary',
+            }}
+          >
+            Your personal Kanban board manager
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            component={RouterLink}
+            to="/signup"
+            size="large"
+            sx={{
+              mt: 2,
+              px: 4,
+              py: 1,
+              fontSize: '1.2rem',
+            }}
+          >
+            Get Started
+          </Button>
+        </Box>
+      </Container>
+    </Box>
   );
 }
-
 export default LandingPage;
